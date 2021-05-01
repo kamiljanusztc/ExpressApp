8ef674dcdf6e59ba82a2f13f05e65988ccade5be
Kamil Janusz <kamiljanusztc@gmail.com>
2021-05-01 16:30:26 +0200

    Add contact form

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
-
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
 const app = express();
 
 app.engine('.hbs', hbs());
@@ -9,8 +10,23 @@ app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+// forms x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.post('/contact/send-message', upload.single('image'), (req, res) => {
+
+  const { author, sender, title, message } = req.body;
+
+  if(author && sender && title && message) {
+    res.render('contact', { isSent: true, fileName: req.file.originalname } );
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
 });
 
 app.get('/hello/:name', (req, res) => {
